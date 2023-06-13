08535d64a1e777962d55a2f66c9c3c8b15cef274
RenatoRios1 <renatoarios1@gmail.com>
2023-06-12 20:49:00 -0500

    Added to duck testing

--- test/models/ducks_test.py
+++ test/models/ducks_test.py
@@ -3,9 +3,39 @@ import unittest
 from src.models import ducks
 
 class DuckTest(unittest.TestCase):
+
+    testName = "TestDuck"
+    testWeight = 100
+    testColor = "Yellow"
+    testsex = "Male"
+
     def test_object_creation(self):
         testDuck = ducks.Duck("test", 100, "green", "Male")
         self.assertIsNotNone(testDuck)
 
-if __name__ == '__name__':
+    #Test your Successes 
+    def test_eat_method_pass(self):
+        testDuck1 = ducks.Duck(self.testName, self.testWeight, self.testColor, self.testsex)
+        testDuck1.eat()
+
+        expected = 101
+        actual = testDuck1.weight
+
+        self.assertEqual(expected, actual)
+
+    #Test to make sure it fails like it supposed to
+    def test_eat_method_fail(self):
+        testDuck1 = ducks.Duck(self.testName, self.testWeight, self.testColor, self.testsex)
+        testDuck1.eat()
+
+        expected = 0
+        actual = testDuck1.weight
+
+        self.assertNotEqual(expected, actual)
+
+    
+    
+
+
+if __name__ == '__main__':
     unittest.main()
